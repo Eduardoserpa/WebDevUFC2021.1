bba1c3af5d6630d23e0fabe7d527ddae8c852aaf
joirneto <joirneto@gmail.com>
2021-08-15 15:15:51 -0300

    Atualização user

--- routes/login-route.js
+++ routes/login-route.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/login', function(req, res, next) {
-  res.render('login')
+  const user = req.cookies.user;  
+  if(user===''){
+    res.render('login',{});
+  }
+  else{
+    res.redirect('/myProfile');
+  }
 })
 
 module.exports = router
