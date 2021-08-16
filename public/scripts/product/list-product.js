c2f60578bf299149ef7c4854040fcefaf32ca197
Eduardo.Serpa <36706433+Eduardoserpa@users.noreply.github.com>
2021-08-16 09:49:47 -0300

    Add files via upload

--- public/scripts/product/list-product.js
+++ public/scripts/product/list-product.js
@@ -1,5 +1,8 @@
+//Read
 $(document).ready(function(){
-    var uri = 'http://localhost:3000/product/';  
+
+    var uri = 'http://localhost:3000/product/';
+  
     fetch(uri, {method: 'GET', mode: 'cors', redirect: 'follow'})
     .then(response => response.text())
     .then(result => {
@@ -8,51 +11,16 @@ $(document).ready(function(){
     .catch(error => console.log('error', error));
 });
 
-function editarProduto(){
-    var id =  event.target.parentNode.parentNode.childNodes[0].innerText;
-    sessionStorage.setItem('id-editar',id);
-    window.location.replace('/addProduct?id=' + id);
-}
-
-function excluirProduto(){
-    var id =  event.target.parentNode.parentNode.childNodes[0].innerText;
-    sessionStorage.setItem('id-delete',id);
-    $('#myModal').modal('show');
-}
-
-function deleteProduct() {
-    var id = sessionStorage.getItem('id-delete');
-    console.log('session ' + id);
-    var uri = `http://localhost:3000/product/` + id;
-
-    var req = new XMLHttpRequest();
-    req.addEventListener("readystatechange", function() {
-        if(this.readyState === 4) {
-            new Promise(() =>{
-                $('#modal-comp').modal('show');
-            });
-        }
-    });
-    req.open("DELETE", uri);
-    req.setRequestHeader('Access-Control-Allow-Origin', '*');
-    req.setRequestHeader('Accept', '*/*');    
-    req.send();
-}
-
-$('#modal-comp').on('hidden.bs.modal', function () {
-    window.location.reload();
-});
-
 function criarTabela(data){
     var container = $('#my-container'),
     table = $('<table>').addClass('table');
     var tr_head = $('<tr>');
-    tr_head.append('<th>' + 'Código' + '</th>');
     tr_head.append('<th>' + 'Produto' + '</th>');
     tr_head.append('<th>' + 'Categoria' + '</th>');
     tr_head.append('<th>' + 'Quantidade em Estoque' + '</th>');
     tr_head.append('<th>' + 'Valor (Unidade)' + '</th>');
     tr_head.append('<th>' + 'Valor (Total)' + '</th>');
+    tr_head.append('<th>' + 'ID' + '</th>');
     tr_head.append('<th>' + 'Ações' + '</th>');
     table.append(tr_head);
   
@@ -60,17 +28,58 @@ function criarTabela(data){
     array.forEach(function(user) {
         var tr = $('<tr>');
         var valorTotal = user.estoque * user.valor;
-        user.total = valorTotal;        
-        [ '_id', 'nome', 'categoria', 'estoque', 'valor','total'].forEach(function(attr) {
+        user.total = valorTotal;
+        
+        [ 'nome', 'categoria', 'estoque', 'valor','total','_id'].forEach(function(attr) {
             tr.append('<td>' + user[attr] + '</td>');
         });
         tr.append(
-            '<td>' +         
-                '<button type="submit" class="btn btn-primary" onClick="excluirProduto()">Excluir</button>' +
-                '<button type="submit" class="btn btn-warning" onClick="editarProduto()">Editar</button>' +
+            '<td class="td-acoes">' +
+                '<a href="editproduct?" rel="lightbox" title="Imagem 6">' +
+                    '<img src="../../imagens/icones/edit-solid.svg" width="20" height="20" alt="" title="Editar" /></a>' +
+                '<a rel="lightbox" title="Imagem 6">' +
+                    '<img src="../../imagens/icones/trash-solid.svg" width="20" height="20" alt="" title="Excluir" /></a>' +
             '</td>'
-        );
+        );      
         table.append(tr);
-    });  
+    });
+  
     container.append(table);
 }
+/*
+function filtrarTabela(data,nomeFiltro,categoriaFiltro){
+    var container = $('#my-container'),
+    table = $('<table>').addClass('table');
+    var tr_head = $('<tr>');
+    tr_head.append('<th>' + 'Produto' + '</th>');
+    tr_head.append('<th>' + 'Categoria' + '</th>');
+    tr_head.append('<th>' + 'Quantidade em Estoque' + '</th>');
+    tr_head.append('<th>' + 'Valor (Unidade)' + '</th>');
+    tr_head.append('<th>' + 'Valor (Total)' + '</th>');
+    tr_head.append('<th>' + 'ID' + '</th>');
+    tr_head.append('<th>' + 'Ações' + '</th>');
+    table.append(tr_head);
+  
+    var array = JSON.parse(data);
+    array.forEach(function(user) {
+        if(nomeFiltro==nome || categoria==filtro){
+            var tr = $('<tr>');
+            var valorTotal = user.estoque * user.valor;
+            user.total = valorTotal;
+            
+            [ 'nome', 'categoria', 'estoque', 'valor','total','_id'].forEach(function(attr) {
+                tr.append('<td>' + user[attr] + '</td>');
+            });
+            tr.append(
+                '<td class="td-acoes">' + 
+                    '<a href="editproduct" rel="lightbox" title="Imagem 6">' +
+                        '<img src="../../imagens/icones/edit-solid.svg" width="20" height="20" alt="" title="Editar" /></a>' +
+                    '<a rel="lightbox" title="Imagem 6">' +
+                        '<img src="../../imagens/icones/trash-solid.svg" width="20" height="20" alt="" title="Excluir" /></a>' + 
+                '</td>'
+            );
+        }
+        table.append(tr);
+    });
+    container.append(table);
+}*/
