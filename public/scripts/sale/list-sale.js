1bd46cf68c36cbadc7b9601ae38c64cab835ed94
Pedro <pedroivo1020@gmail.com>
2021-08-31 20:43:23 -0300

    filter-sale

--- public/scripts/sale/list-sale.js
+++ public/scripts/sale/list-sale.js
@@ -1,18 +1,30 @@
 let NumeroLinhasPorPagina = 6;
 let IndexPagina = 1;
 
-$(document).ready(() => getSales());
+$(document).ready(() => getSales(`http://localhost:3000/sale/${NumeroLinhasPorPagina}/${IndexPagina}`));
 $("#PreviousPage").click(function() {
     IndexPagina --;
-    getSales();
+    var uri = `http://localhost:3000/sale/${NumeroLinhasPorPagina}/${IndexPagina}`;
+    getSales(uri);
 });
 $("#NextPage").click(function() {
     IndexPagina ++;
-    getSales();
+    var uri = `http://localhost:3000/sale/${NumeroLinhasPorPagina}/${IndexPagina}`;
+    getSales(uri);
 });
 
-function getSales(){
-    var uri = `http://localhost:3000/sale/${NumeroLinhasPorPagina}/${IndexPagina}`;  
+$("#consultar-btn").click(function() {
+    var produto = $('#produto').val();
+    var vendedor = $('#vendedor').val();
+    var uri = `http://localhost:3000/sale/`;
+
+    if(produto) uri += `?produto=${produto.split(" ")[2]}`;
+    if(categoria) uri += `?categoria=${vendedor}`;
+
+    getSales(uri);
+});
+
+function getSales(uri){
     fetch(uri, {method: 'GET', mode: 'cors', redirect: 'follow'})
     .then(response => response.text())
     .then(result => {
@@ -45,6 +57,41 @@ function criarPagination(pageIndex, pageCount, hasPreviousPage, hasNextPage){
     }
     pagination.append(linha);
 }
+
+function editarSale(){
+    var id =  event.target.parentNode.parentNode.childNodes[0].innerText;
+    sessionStorage.setItem('id-editar',id);
+    window.location.replace('/addSale?id=' + id);
+}
+
+function excluirSale(){
+    var id =  event.target.parentNode.parentNode.childNodes[0].innerText;
+    sessionStorage.setItem('id-delete',id);
+    $('#myModal').modal('show');
+}
+
+function deleteSale() {
+    var id = sessionStorage.getItem('id-delete');
+    console.log('session ' + id);
+    var uri = `http://localhost:3000/sale/` + id;
+
+    var req = new XMLHttpRequest();
+    req.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            new Promise(() =>{
+                $('#modal-comp').modal('show');
+            });
+        }
+    });
+    req.open("DELETE", uri);
+    req.setRequestHeader('Access-Control-Allow-Origin', '*');
+    req.setRequestHeader('Accept', '*/*');    
+    req.send();
+}
+
+$('#modal-comp').on('hidden.bs.modal', function () {
+    window.location.reload();
+});
 
 function criarTabela(data){
     var container = $('#my-container'),
